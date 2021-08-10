c31c44847f5fefe91c5be8064f198d10552f79d3
mirrorbottelegram <88614289+mirrorbottelegram@users.noreply.github.com>
2021-08-10 15:24:32 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,5 +1,5 @@
 aria2c --enable-rpc --check-certificate=false \
-   --max-connection-per-server=10 --rpc-max-request-size=1024M \
+   --max-connection-per-server=10 --rpc-max-request-size=2024M \
    --bt-stop-timeout=1200 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
